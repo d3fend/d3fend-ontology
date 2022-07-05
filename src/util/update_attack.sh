b8704d0a3f6bee5b315e358424c4829dda0b5c26
Peter Kaloroumakis <pk@mitre.org>
2022-07-05 11:27:48 -0400

    make sed command work for gnu and bsd sed and clean up temp files

--- src/util/update_attack.sh
+++ src/util/update_attack.sh
@@ -4,10 +4,11 @@ pipenv run python src/util/update_attack.py
     --template reports/attack_update-missing_attack_ids-robot_template.csv \
     --ontology-iri http://d3fend.mitre.org/ontologies/d3fend.owl \
     --prefix "d3f: http://d3fend.mitre.org/ontologies/d3fend.owl#" \
-    -o src/ontology/attack_update.ttl
+    -o src/ontology/_attack_update.ttl
 
 # remove robot's uncessary type annotation
-sed -i  -e 's/\^\^xsd\:string//g' src/ontology/attack_update.ttl
+# works on both bsd and gnu sed
+sed -e "s/\^\^xsd\:string//g" src/ontology/_attack_update.ttl > src/ontology/attack_update.ttl
 
 ./bin/robot merge \
     -i src/ontology/d3fend-protege.ttl \
@@ -18,3 +19,13 @@ pipenv run ttlfmt src/ontology/d3fend-protege.attack_update.ttl
 
 echo Created new ontology file with updates here: src/ontology/d3fend-protege.attack_update.ttl
 echo Please manually review and compare to: src/ontology/d3fend-protege.ttl
+
+
+function cleanup(){
+    echo -e "\t deleting: $1"
+    rm -f $1
+}
+
+echo cleaning up:
+cleanup src/ontology/_attack_update.ttl
+cleanup src/ontology/attack_update.ttl
